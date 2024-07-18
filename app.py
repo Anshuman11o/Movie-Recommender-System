b0b5472c0d3ee2fff53215521f4557d42d21d2cf
Anshuman11o <107318792+Anshuman11o@users.noreply.github.com>
2024-07-18 02:02:05 -0400

    app.py
    Added the standard features

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import streamlit as st
 import requests
 
 st.header("Movies Recommendation System Using Machine Learning")
+movies = pickle.load(open('artificates/movies_list.pkl', 'rb'))
+similarity = pickle.load(open('artificates/similarity.pkl', 'rb'))
+
+
+movie_list = movies['title'].values
+st.selectbox(
+    'Type or select a movie to get a recommendation',
+    movie_list
+    )
