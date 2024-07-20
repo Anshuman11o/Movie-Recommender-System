ee490b0ba371e52fe18d5aadff25a6316d797421
Anshuman11o <107318792+Anshuman11o@users.noreply.github.com>
2024-07-20 09:28:54 -0400

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ st.header("Movies Recommendation System Using Machine Learning")
 
 movies = pickle.load(open('artifacts/movies.pkl.lzma', 'rb'))
 similarity = pickle.load(open('artifacts/similarity.pkl.lzma', 'rb'))
-movie_list = movies[ 'title'].values
+
+movie_list = movies['title'].values
 st.selectbox(
     'Type or select a movie to get recommendation', 
     movie_list
